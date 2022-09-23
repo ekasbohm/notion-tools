225729b6ece24af52c0e3cc0fd316bb1dfcde621
Emily Kasbohm <emily_kasbohm@brown.edu>
2022-09-23 15:03:17 -0400

    Initial next setup

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Notion Tools</title>
+      </Head>
+
+      <main>
+        <h1>Hi</h1>
+      </main>
+    </>
+  );
+};
+
+export default Home;
